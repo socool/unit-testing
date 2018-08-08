70f898a07937effc8fdd3690ad56e6da7d8d4681
Kamolsit Mongkolsrisawat <socool@Kamolsits-MacBook-Pro.local>
2018-08-08 22:32:22 +0700

    add allItemFromDb case

--- src/test/java/com/bcorpse/unittesing/controller/ItemControllerTest.java
+++ src/test/java/com/bcorpse/unittesing/controller/ItemControllerTest.java
@@ -14,6 +14,8 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.Arrays;
+
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -56,6 +58,28 @@ public class ItemControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content()
                         .json("{id:1,name:Ball,price:10,quantity:10}"))
+                .andReturn();
+
+        // verify
+
+    }
+
+    @Test
+    public void allItemFromDb() throws Exception{
+        when(itemBuzzService.retreiveAllItems()).thenReturn(
+                Arrays.asList(new Item(1,"Ball",10,10),
+                        new Item(2,"Glove",5,25),
+                        new Item(3,"Net",3,8))
+        );
+        // call "/hello-world"
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/all-items-from-db")
+                .accept(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .json("[{id:1,name:Ball,price:10,quantity:10},{id:2,name:Glove,price:5,quantity:25},{id:3,name:Net,price:3,quantity:8}]"))
                 .andReturn();
 
         // verify
