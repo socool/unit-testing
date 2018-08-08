7a659d47d23ad875b7ddaf0595db2a4aca82f6df
Kamolsit Mongkolsrisawat <socool@Kamolsits-MacBook-Pro.local>
2018-08-08 21:15:52 +0700

    add itemFromBuzzServiceBasic

--- src/test/java/com/bcorpse/unittesing/unittesting/controller/ItemControllerTest.java
+++ src/test/java/com/bcorpse/unittesing/unittesting/controller/ItemControllerTest.java
@@ -1,6 +1,7 @@
 package com.bcorpse.unittesing.unittesting.controller;
 
 import com.bcorpse.unittesing.unittesting.business.ItemBuzzService;
+import com.bcorpse.unittesing.unittesting.model.Item;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -31,11 +33,29 @@ public class ItemControllerTest {
         RequestBuilder request = MockMvcRequestBuilders
                 .get("/dummy-item")
                 .accept(MediaType.APPLICATION_JSON);
-        MvcResult result = mockMvc
-                .perform(request)
+
+        MvcResult result = mockMvc.perform(request)
                 .andExpect(status().isOk())
                 .andExpect(content()
                         .json("{\"id\":1,\"name\":\"Ball\",\"price\":10,\"quantity\":10}"))
+                .andReturn();
+
+        // verify
+
+    }
+
+    @Test
+    public void itemFromBuzzServiceBasic() throws Exception{
+        when(itemBuzzService.retrieveHardcodeItem()).thenReturn(new Item(1,"Ball",10,10));
+        // call "/hello-world"
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/item-from-buzz")
+                .accept(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .json("{id:1,name:Ball,price:10,quantity:10}"))
                 .andReturn();
 
         // verify
