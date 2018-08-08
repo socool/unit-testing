5b5d4f90be2682a1629ea3b09183ee2a2d565e97
Kamolsit Mongkolsrisawat <socool@Kamolsits-MacBook-Pro.local>
2018-08-09 01:29:17 +0700

    add ItemRepositoryTest for JPA test

---
+++ src/test/java/com/bcorpse/unittesing/business/ItemRepositoryTest.java
@@ -0,0 +1,26 @@
+package com.bcorpse.unittesing.business;
+
+import com.bcorpse.unittesing.data.ItemRepository;
+import com.bcorpse.unittesing.model.Item;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class ItemRepositoryTest {
+    @Autowired
+    private ItemRepository repository;
+
+    @Test
+    public void testFindAll(){
+        List<Item> items = repository.findAll();
+        assertEquals(3,items.size());
+    }
+}
